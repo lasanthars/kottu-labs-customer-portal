e19f934e2aeea4a619a43cba4b5ff48842e5b996
nadun <nadun.rubasinghe@pearson.com>
2018-02-22 08:02:31 +0530

    resolving errors

--- src/app/components/SignatureKottuMenu.ts
+++ src/app/components/SignatureKottuMenu.ts
@@ -31,20 +31,28 @@ export class SignatureKottuMenuComponent {
     this.getAllSignatureMenus();
   }
 
-  convertToTotal(menu: any, index: number, quantity: any, type: string) {
-    if (type === 'numeric') {
-      if (quantity === undefined || quantity === '') {
-        this.menus[index].setmenu = price * 1;
-      } else {
-        this.total = price * quantity;
-      }
-    } else {
-      if (quantity === undefined || quantity === '') {
-        this.total = price[0].price * 1;
-      } else {
-        this.total = price[0].price * quantity;
-      }
-    }
+  // convertToTotal(menu: any, index: number, quantity: any, type: string) {
+  //   if (type === 'numeric') {
+  //     if (quantity === undefined || quantity === '') {
+  //       this.menus[index].setmenu = price * 1;
+  //     } else {
+  //       this.total = price * quantity;
+  //     }
+  //   } else {
+  //     if (quantity === undefined || quantity === '') {
+  //       this.total = price[0].price * 1;
+  //     } else {
+  //       this.total = price[0].price * quantity;
+  //     }
+  //   }
+  // }
+
+  convertTotalPortion(event:any, menu: any, index: number, model: number, type: string) {
+
+  }
+
+  convertToTotalQuantity(menu: any, index: number, model: number, type: string) {
+
   }
 
   // changePortion(event: any, obj: object, index: number, quantity: number) {
